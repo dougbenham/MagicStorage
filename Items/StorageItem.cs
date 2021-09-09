2dfd7309fedddc2d96be814278bc329e20e07c04
Exterminatorx99 <kristijan.kos99@gmail.com>
2021-09-09 16:36:04 +0200

    Add mod exchange recipes

--- Items/StorageItem.cs
+++ Items/StorageItem.cs
@@ -8,7 +8,24 @@ namespace MagicStorageExtra.Items
 		{
 			AddRecipe(this);
 			if (MagicStorageExtra.MagicStorage != null)
-				AddRecipe(MagicStorageExtra.MagicStorage.GetItem(Name));
+			{
+				ModItem modItem = MagicStorageExtra.MagicStorage.GetItem(Name);
+				AddRecipe(modItem);
+				AddReverseRecipes(modItem);
+			}
+		}
+
+		public void AddReverseRecipes(ModItem other)
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(this);
+			recipe.SetResult(other);
+			recipe.AddRecipe();
+
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(other);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
 		}
 
 		public virtual void AddRecipe(ModItem result)
