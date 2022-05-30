3f0b15fad9b480c66386c1947cd90159fc286ac7
Doug Benham <douglasabenham@gmail.com>
2022-05-29 18:38:59 -0700

    Dropping an item into the recipes, stored ingredients or the result slot will deposit the item into storage
    This is a helpful change because we need as many ways as possible to deposit items into storage.

--- CraftingGUI.cs
+++ CraftingGUI.cs
@@ -1485,6 +1485,9 @@ namespace MagicStorageExtra
 
 		private static void HoverRecipe(int slot, ref int hoverSlot)
 		{
+			if (TryDepositMouseItem())
+				return;
+
 			int visualSlot = slot;
 			slot += numColumns * (int) Math.Round(scrollBar.ViewPosition);
 			if (slot < recipes.Count)
@@ -1533,6 +1536,22 @@ namespace MagicStorageExtra
 
 				hoverSlot = visualSlot;
 			}
+		}
+
+		private static bool TryDepositMouseItem()
+		{
+			Player player = Main.LocalPlayer;
+			if (MouseClicked && !Main.mouseItem.IsAir && player.itemAnimation == 0 && player.itemTime == 0)
+			{
+				if (TryDepositResult(Main.mouseItem))
+				{
+					RefreshItems();
+					Main.PlaySound(SoundID.Grab);
+					return true;
+				}
+			}
+
+			return false;
 		}
 
 		private static void SetSelectedRecipe(Recipe recipe)
@@ -1625,6 +1644,9 @@ namespace MagicStorageExtra
 
 		private static void HoverStorage(int slot, ref int hoverSlot)
 		{
+			if (TryDepositMouseItem())
+				return;
+
 			int visualSlot = slot;
 			slot += numColumns2 * (int) Math.Round(scrollBar2.ViewPosition);
 			if (slot < storageItems.Count)
@@ -1659,7 +1681,7 @@ namespace MagicStorageExtra
 			if (MouseClicked)
 			{
 				bool changed = false;
-				if (!Main.mouseItem.IsAir && player.itemAnimation == 0 && player.itemTime == 0 && result != null && Main.mouseItem.type == result.type)
+				if (!Main.mouseItem.IsAir && player.itemAnimation == 0 && player.itemTime == 0)
 				{
 					if (TryDepositResult(Main.mouseItem))
 						changed = true;
